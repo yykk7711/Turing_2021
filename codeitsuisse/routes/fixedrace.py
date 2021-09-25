b7eb863333f623fa5531f4292ed93e6061f99ab5
richardbryan <richard.bryan1680@gmail.com>
2021-09-25 18:27:26 +0800

    fixed race testing

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -8,7 +8,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
+@app.route('/fixedrace', methods=['POST'])
 
 def evaluateFixedrace():
     data = request.get_json()
