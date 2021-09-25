6faf57e99e0e2c8da5571b11f529c0f21c678a0f
Alan Ngo <45725511+yykk7711@users.noreply.github.com>
2021-09-25 20:07:42 +0800

    optopt

---
+++ codeitsuisse/routes/optopt.py
@@ -0,0 +1,21 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/optopt', methods=['POST'])
+
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input")
+
+    result = inputValue * inputValue
+
+    logging.info("My result :{}".format(result))
+    
+    return json.dumps(result)
