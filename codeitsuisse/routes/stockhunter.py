d947edbb8750a09bda87d2046aa32fdd8febf527
Zach <67416202+zachkongoyu@users.noreply.github.com>
2021-09-25 22:10:35 +0800

    j

--- codeitsuisse/routes/stockhunter.py
+++ codeitsuisse/routes/stockhunter.py
@@ -12,46 +12,49 @@ logger = logging.getLogger(__name__)
 def evaluate_stockhunter():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data[0]
+    
+    output = []
 
-    riskindex = dict()
-    risklevel = dict()
-    riskcost = dict()
+    for inputValue in data:
 
-    for i in range(inputValue['targetPoint']['first']+1):
-        for j in range(inputValue['targetPoint']['second']+1):
-            if i == 0 and j == 0 or (i == inputValue['targetPoint']['first'] and j == inputValue['targetPoint']['second']):
-                riskindex[f"{i}{j}"] = 0
-            elif i == 0:
-                riskindex[f"{i}{j}"] = j*inputValue['verticalStepper']
-            elif j == 0:
-                riskindex[f"{i}{j}"] = i*inputValue['horizontalStepper']
+        riskindex = dict()
+        risklevel = dict()
+        riskcost = dict()
+
+        for i in range(inputValue['targetPoint']['first']+1):
+            for j in range(inputValue['targetPoint']['second']+1):
+                if i == 0 and j == 0 or (i == inputValue['targetPoint']['first'] and j == inputValue['targetPoint']['second']):
+                    riskindex[f"{i}{j}"] = 0
+                elif i == 0:
+                    riskindex[f"{i}{j}"] = j*inputValue['verticalStepper']
+                elif j == 0:
+                    riskindex[f"{i}{j}"] = i*inputValue['horizontalStepper']
+                else:
+                    riskindex[f"{i}{j}"] = risklevel[f"{i-1}{j}"] * risklevel[f"{i}{j-1}"]
+
+                risklevel[f"{i}{j}"] = (riskindex[f"{i}{j}"] + inputValue['gridDepth']) % inputValue['gridKey']
+
+                if risklevel[f"{i}{j}"] % 3 == 0:
+                    riskcost[f"{i}{j}"] = 3
+                elif risklevel[f"{i}{j}"] % 3 == 1:
+                    riskcost[f"{i}{j}"] = 2
+                else:
+                    riskcost[f"{i}{j}"] = 1
+
+        grid = [[] for i in range(inputValue['targetPoint']['second']+1)]
+
+        for k,v in riskcost.items():
+            if v == 3:
+                grid[int(k[1])].append('L')
+            elif v == 2:
+                grid[int(k[1])].append('M')
             else:
-                riskindex[f"{i}{j}"] = risklevel[f"{i-1}{j}"] * risklevel[f"{i}{j-1}"]
+                grid[int(k[1])].append('S')
 
-            risklevel[f"{i}{j}"] = (riskindex[f"{i}{j}"] + inputValue['gridDepth']) % inputValue['gridKey']
-
-            if risklevel[f"{i}{j}"] % 3 == 0:
-                riskcost[f"{i}{j}"] = 3
-            elif risklevel[f"{i}{j}"] % 3 == 1:
-                riskcost[f"{i}{j}"] = 2
-            else:
-                riskcost[f"{i}{j}"] = 1
-
-    grid = [[] for i in range(inputValue['targetPoint']['second']+1)]
-
-    for k,v in riskcost.items():
-        if v == 3:
-            grid[int(k[1])].append('L')
-        elif v == 2:
-            grid[int(k[1])].append('M')
-        else:
-            grid[int(k[1])].append('S')
-
-    output = [{
-        "gridMap": grid,
-        "minimumCost": 9
-    }]
+        output.append({
+            "gridMap": grid,
+            "minimumCost": 9
+        })
 
     logging.info("My result :{}".format(output))
     logging.info(json.dumps(output))
